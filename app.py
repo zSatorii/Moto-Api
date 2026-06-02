5073630c29d4003527bbb25fd68402c91f7dc44c
zSatorii <johanikaros@gmail.com>
2026-06-02 20:14:26 +0000

    Fix-Se arregla el error forzado

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def obtener_peritajes():
 
 @app.route('/api/inventario', methods=['GET'])
 def gestionar_inventario():
-    return {"status": "en desarrollo", "modulo": "inventario"}, 200
+	return {"status": "en desarrollo", "modulo": "inventario"}, 200
 
 
 if __name__ == "__main__":
